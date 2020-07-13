4acfd913930e445ab5029d8991e13aa6fc0de99d
Andrej Kolar <andrej.kolar@me.com>
2020-07-13 16:54:34 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const PushNotifications = require("node-pushnotifications");
+const prompts = require("prompts");
+
+const questions = [
+  {
+    type: "text",
+    name: "keyId",
+    message: "Input your keyId:",
+  },
+  {
+    type: "text",
+    name: "teamId",
+    message: "Input your teamId:",
+  },
+  {
+    type: "text",
+    name: "key",
+    message: "Input your p8 path:",
+  },
+  {
+    type: "text",
+    name: "bundleId",
+    message: "Input your bundle id:",
+  },
+  {
+    type: "text",
+    name: "pushToken",
+    message: "Input your APNS token:",
+  },
+  {
+    type: "text",
+    name: "title",
+    message: "Enter message title:",
+  },
+  {
+    type: "text",
+    name: "body",
+    message: "Enter message body:",
+  },
+];
+
+(async () => {
+  const response = await prompts(questions);
+
+  const { key, pushToken, teamId, keyId, bundleId, title, body } = response;
+
+  const settings = {
+    apn: {
+      token: {
+        key,
+        keyId,
+        teamId,
+      },
+      production: false,
+    },
+  };
+  const push = new PushNotifications(settings);
+
+  const registrationIds = pushToken;
+
+  const data = {
+    topic: bundleId,
+    title,
+    body,
+  };
+
+  const results = await push.send(registrationIds, data);
+  console.log(JSON.stringify(results, 0, 2));
+  process.exit();
+})();
